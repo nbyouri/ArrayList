fce9026d4f3861f4d3f52c6dee93a25aa1165ef4
yrmt <youri.mout@gmail.com>
2015-03-16 00:14:32 +0100

    Add DragonFly support.

--- taskmanager/examples.c
+++ taskmanager/examples.c
@@ -1,4 +1,6 @@
 #include <dirent.h>
+#include <inttypes.h>
+#include <stdbool.h>
 #include <err.h>
 #include <errno.h>
 #include <pwd.h>
@@ -7,17 +9,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/param.h>
+#include <paths.h>
 #include <sys/sched.h>
 #include <sys/sysctl.h>
 #include <unistd.h>
+#include <kinfo.h>
 #include <kvm.h>
+#ifdef __NetBSD__
 #include <sys/proc.h>
+#else
+#include <sys/user.h>
+#endif
 #include <sys/vmmeter.h>
+#include <fcntl.h>
+#ifndef __DragonFly__
 #include <sys/swap.h>
+#endif
 #include "tools.h"
 #include "list.h"
 
 extern int errno;
+
+#ifdef __DragonFly__
+#define SDEAD	208
+#define kinfo_proc2	kinfo_proc
+#endif
 
 char    *state_abbrev[] = {
         "", "start", "run", "sleep", "stop", "zomb", "dead", "onproc"
@@ -39,21 +55,22 @@ void get_task_list(ArrayList * list) {
         char *buf = NULL;
         unsigned int i = 0;
         int nproc = 0;
-        //size_t size = sizeof(*kp);
 
 
         /* get process number, stolen from ps */
         /* example with kvm functions */
         char errbuf[_POSIX2_LINE_MAX];
-        kvm_t *kdp = kvm_openfiles(NULL, NULL, NULL, KVM_NO_FILES, errbuf);
+        kvm_t *kdp = kvm_openfiles(_PATH_DEVNULL, NULL, NULL, O_RDONLY, errbuf);
         if (kdp == NULL)
                 errx(1, "%s", errbuf);
 
         /* fill kinfo_proc2 structure */
         /* XXX does it return sleeping processes ? */
-#ifdef __OpenBSD__
+#ifdef	__OpenBSD__
 	if (!(kp = kvm_getprocs(kdp, KERN_PROC_ALL, 0,
-	        	sizeof(struct kinfo_proc), &nproc)))
+		    sizeof(struct kinfo_proc), &nproc)))
+#elif __DragonFly__
+	if (!(kp = kvm_getprocs(kdp, KERN_PROC_ALL, 0, &nproc)))
 #else
         if (!(kp = kvm_getproc2(kdp, KERN_PROC_ALL, 0,
                     sizeof(struct kinfo_proc2), &nproc)))
@@ -61,64 +78,65 @@ void get_task_list(ArrayList * list) {
                 err(1, "%s", kvm_geterr(kdp));
 
         for (i = 0; i < nproc; i++) {
+#ifdef	__DragonFly__
+		if (kp[i].kp_pid >= PID_MAX || kp[i].kp_pid < 1)
+			continue; /* not valid */
+#else
+		if (kp[i].p_pid >= PID_MAX || kp[i].p_pid < 1)
+			continue; /* not valid */
+#endif
+
                 /* get per-process information in our entry */
 #ifdef __OpenBSD__
 		struct kinfo_proc p = kp[i];
-#else 
+#else
                 struct kinfo_proc2 p = kp[i];
 #endif
 
                 list->obj = new(i);
 
+#ifdef	__DragonFly__
+                list->obj->pid = p.kp_pid;
+                list->obj->ppid = p.kp_ppid;
+                list->obj->uid = p.kp_uid;
+                list->obj->prio = p.kp_nice;
+                list->obj->vsz = p.kp_vm_dsize + p.kp_vm_ssize + p.kp_vm_tsize;
+                list->obj->vsz *= getpagesize();
+                list->obj->rss = p.kp_vm_rssize * getpagesize();
+                snprintf(list->obj->state, sizeof(list->obj->state), "%s",
+                    state_abbrev[p.kp_stat]);
+                strlcpy(list->obj->name, p.kp_comm, strlen(p.kp_comm) + 1);
+#else
                 list->obj->pid = p.p_pid;
                 list->obj->ppid = p.p_ppid;
                 list->obj->uid = p.p_uid;
-                list->obj->prio = p.p_priority - PZERO;
+	        list->obj->prio = p.p_priority - PZERO;
                 list->obj->vsz = p.p_vm_dsize + p.p_vm_ssize + p.p_vm_tsize;
                 list->obj->vsz *= getpagesize();
                 list->obj->rss = p.p_vm_rssize * getpagesize();
                 snprintf(list->obj->state, sizeof(list->obj->state), "%s",
                     state_abbrev[p.p_stat]);
                 strlcpy(list->obj->name, p.p_comm, strlen(p.p_comm) + 1);
-                if (!P_ZOMBIE(&p)) { /* XXX much less relax than OpenBSD's */
-                //if (!(p.p_stat == SDEAD)) {
+#endif
+                if (!(p.kp_stat == SDEAD)) { /* same as OpenBSD P_ZOMBIE */
                         /* get process args */
-                        /* example with kvm functions */
-
-#ifdef __OpenBSD__
+#if defined(__OpenBSD__) || defined(__DragonFly__)
 			args = kvm_getargv(kdp, &kp[i], BUFSIZ);
 #else
                         args = kvm_getargv2(kdp, &kp[i], BUFSIZ);
 #endif
-                        /* example with sysctl directly */
-                        /*
-                        mib[0] = CTL_KERN;
-                        mib[1] = KERN_PROC_ARGS;
-                        mib[2] = list->obj->pid;
-                        mib[3] = KERN_PROC_ARGV;
-
-                        if (sysctl(mib, 4, NULL, &size, NULL, 0) < 0)
-                                errx(1, "failed to get kern.proc_args");
-
-                        args = malloc(size * sizeof(char *));
-                        for (int i = 0; i < size; i++) {
-                                args[i] = malloc(BUFSIZ);
-                        }
-
-                        strcpy(args[0], "test");
-                        if (sysctl(mib, 4, args, &size, NULL, 0) < 0)
-                                errx(1, "failed to get kern.proc_args");
-                        size = 5 *  size / 4;
-                        */
-
                         if (args != NULL) {
                                 buf = strjoin(" ", args);
                                 /* XXX free args */
                                 strlcpy(list->obj->cmdline, buf,
                                     sizeof(list->obj->cmdline));
                         }
+#ifdef __DragonFly__
+			list->obj->cpu_user = 0; /* TODO */
+#else
                         list->obj->cpu_user = (100.0 *
                             ((double) p.p_pctcpu / FSCALE));
+#endif
                         list->obj->cpu_system = 0; /* XXX TODO */
                         passwdp = getpwuid(list->obj->uid);
                         if (passwdp != NULL && passwdp->pw_name != NULL)
@@ -133,51 +151,61 @@ void get_task_list(ArrayList * list) {
         buf = NULL;
         free(kp);
         kp = NULL;
-        //if (kvm_close(kdp) == -1)
-        //        errx(1, "failed to close kvm");
  }
 
-int sleeping(unsigned int pid) {
-        int mib[6];
-#ifdef __OpenBSD__
-        struct kinfo_proc kp;
-#else	
-	struct kinfo_proc2 kp;
-#endif
-        size_t size = sizeof(kp);
-        mib[0] = CTL_KERN;
-#ifdef __OpenBSD__
-        mib[1] = KERN_PROC;
+bool sleeping(unsigned int pid) {
+	kvm_t	*kd;
+	struct kinfo_proc *kp;
+	int cnt;
+	int ret;
+
+	kd = kvm_openfiles(_PATH_DEVNULL, _PATH_DEVNULL, NULL, O_RDONLY, NULL);
+	if (kd == NULL)
+		return false;
+
+	kp = kvm_getprocs(kd, KERN_PROC_PID, pid, &cnt);
+	if (kp == NULL) {
+		kvm_close(kd);
+		return false;
+	}
+
+#if __DragonFly__
+	ret = (kp->kp_stat == SSTOP);
 #else
-	mib[1] = KERN_PROC2;
+	ret = (kp->p_stat == SSTOP);
 #endif
-        mib[2] = KERN_PROC_PID;
-        mib[3] = pid;
-        mib[4] = size;
-        mib[5] = 1;
-
-        if (sysctl(mib, 6, &kp, &size, NULL, 0) < 0)
-                errx(1, "could not read kern.proc2 for pid %u\n", pid);
-
-        return (kp.p_stat == SSTOP);
+	kvm_close(kd);
+	return ret;
 }
 
 /* needs big timeout */
 int get_cpu_usage(short *cpu_count, float *cpu_user, float *cpu_system) {
         static long cur_user = 0, cur_system = 0, cur_total = 0;
         static long old_user = 0, old_system = 0, old_total = 0;
+	uint64_t cp_time[CPUSTATES];
+#ifdef	__DragonFly__
+       int mib[2];
+       struct kinfo_cputime percpu;
+       size_t size = sizeof(percpu);
 
-#ifdef __OpenBSD__
-        int mib[] = { CTL_KERN, KERN_CPTIME };
+       if (sysctlbyname("kern.cputime", &percpu, &size, NULL, 0) < 0)
+               errx(1, "failed to sysctl kern.cputime");
+
+       cp_time[CP_USER] = percpu.cp_user;
+       cp_time[CP_NICE] = percpu.cp_nice;
+       cp_time[CP_SYS]  = percpu.cp_sys;
+       cp_time[CP_INTR] = percpu.cp_intr;
+       cp_time[CP_IDLE] = percpu.cp_idle;
 #else
+#ifdef	__OpenBSD__
+        int mib[] = { CTL_KERN, KERN_CPTIME };
+#elif	__NetBSD__
 	int mib[] = { CTL_KERN, KERN_CP_TIME };
 #endif
-        long cp_time[CPUSTATES];
-        size_t size = sizeof(cp_time);
-
         if (sysctl(mib, 2, &cp_time, &size, NULL, 0) < 0)
                 errx(1, "failed to get kern.cp_time");
 
+#endif
         old_user = cur_user;
         old_system = cur_system;
         old_total = cur_total;
@@ -207,23 +235,46 @@ int get_cpu_usage(short *cpu_count, float *cpu_user, float *cpu_system) {
 
 int get_memory_usage (uint64_t *memory_total, uint64_t *memory_free, uint64_t *memory_cache, uint64_t *memory_buffers, uint64_t *swap_total, uint64_t *swap_free)
 {
+#ifdef __OpenBSD__
+	int mib[] = {CTL_VM, VM_UVMEXP};
+	struct uvmexp uvmexp;
+#elif __NetBSD__
         int mib[] = {CTL_VM, VM_METER};
+#endif
         struct vmtotal vmtotal;
 
+#ifndef __DragonFly__
         struct swapent *swdev;
-        int nswap, i;
+        int nswap;
+#endif
         size_t size;
 
+#ifdef __OpenBSD__
+	size = sizeof(uvmexp);
+	if (sysctl(mib, 2, &uvmexp, &size, NULL, 0) < 0)
+		errx(1,"failed to get vm.uvmexp");
+	/* cheat : rm = tot used, add free to get total */
+	*memory_free = pagetok((guint64)uvmexp.free);
+	*memory_total = pagetok((guint64)uvmexp.npages);
+	*memory_cache = 0;
+	*memory_buffers = 0; /*pagetok(uvmexp.npages - uvmexp.free - uvmexp.active);*/
+#else
         size = sizeof(vmtotal);
+#ifdef __DragonFly__
+	if (sysctlbyname("vm.vmstats", &vmtotal, &size, NULL, 0) < 0)
+#else
         if (sysctl(mib, 2, &vmtotal, &size, NULL, 0) < 0)
+#endif
                 errx(1,"failed to get vm.meter");
         /* cheat : rm = tot used, add free to get total */
         *memory_total = pagetok(vmtotal.t_rm + vmtotal.t_free);
         *memory_free = pagetok(vmtotal.t_free);
         *memory_cache = 0;
         *memory_buffers = pagetok(vmtotal.t_rm - vmtotal.t_arm);
+#endif
 
         /* get swap stats */
+#ifndef __DragonFly__
         if ((nswap = swapctl(SWAP_NSWAP, 0, 0)) == 0)
                 errx(1,"failed to get swap device count");
 
@@ -234,93 +285,110 @@ int get_memory_usage (uint64_t *memory_total, uint64_t *memory_free, uint64_t *m
                 free(swdev);
                 errx(1,"failed to get swap stats");
         }
+	/* Total things up */
+	*swap_total = *swap_free = 0;
+	int i;
+	for (i = 0; i < nswap; i++) {
+		if (swdev[i].se_flags & SWF_ENABLE) {
+			*swap_free += (swdev[i].se_nblks - swdev[i].se_inuse);
+			*swap_total += swdev[i].se_nblks;
+		}
+	}
+	*swap_total *= DEV_BSIZE;
+	*swap_free *= DEV_BSIZE;
+	free(swdev);
+#else /* DragonFly */
+	int temp_swap_total, temp_swap_used;
+	size_t len = sizeof(int);
 
-        /* Total things up */
-        *swap_total = *swap_free = 0;
-        for (i = 0; i < nswap; i++) {
-                if (swdev[i].se_flags & SWF_ENABLE) {
-                        *swap_free += (swdev[i].se_nblks - swdev[i].se_inuse);
-                        *swap_total += swdev[i].se_nblks;
-                }
-        }
-        *swap_total *= DEV_BSIZE;
-        *swap_free *= DEV_BSIZE;
-        free(swdev);
-        return 0;
+	if (sysctlbyname("vm.swap_size", &temp_swap_total, &len, NULL, 0) < 0)
+		errx(1, "failed to get vm.swap_size");
+
+	if (sysctlbyname("vm.swap_anon_use", &temp_swap_used, &len, NULL, 0) < 0)
+		errx(1, "failed to get vm.swap_anon_use");
+
+#define CONVERT(v)     ((quad_t)(v) * (getpagesize() / 1024))
+
+	*swap_total = CONVERT(temp_swap_total);
+	*swap_free = CONVERT(temp_swap_total - temp_swap_used);
+
+
+#endif
+	return 0;
 }
 
 int main(void) {
-        /*
-         *
-         *
-         * Get task list and
-         * assign to a list.
-         *
-         * works.
-         */
-        ArrayList *list;
+	/*
+	 *
+	 *
+	 * Get task list and
+	 * assign to a list.
+	 *
+	 * works.
+	 */
+	ArrayList *list;
 
-        list = malloc(sizeof(*list));
+	list = malloc(sizeof(*list));
 
-        initList(list);
+	initList(list);
 
-        get_task_list(list);
+	get_task_list(list);
 
-        printf("\n\nPROC_LIST\n\n");
-        foreach(list) {
-                toString(list->obj);
-                /*
-                 *
-                 * is a pid sleeping ?
-                 *
-                 * not sure if working.
-                 *
-                 */
-                printf("-> sleeping ? %s\n", sleeping(list->obj->pid) ? "yes" : "no");
-        }
+	printf("\n\nPROC_LIST\n\n");
+	foreach(list) {
+		toString(list->obj);
+		/*
+		 *
+		 * is a pid sleeping ?
+		 *
+		 * not sure if working.
+		 *
+		 */
+		printf("-> sleeping ? %s\n", sleeping(list->obj->pid) ? "yes" : "no");
+	}
 
-        cleanList(list);
+	cleanList(list);
 
-        if (!isEmpty(list)) {
-                printf("list is supposed to be empty!\n");
-        }
+	if (!isEmpty(list)) {
+		printf("list is supposed to be empty!\n");
+	}
 
-        free(list);
-        list = NULL;
+	free(list);
+	list = NULL;
 
-        /*
-         *
-         * Get cpu usage info.
-         *
-         * seems to work, verify values.
-         */
-        printf("\n\nCPU_INFO\n\n");
-        short cpu_count = 0;
-        float cpu_user = 0;
-        float cpu_system = 0;
+	/*
+	 *
+	 * Get cpu usage info.
+	 *
+	 * seems to work, verify values.
+	 */
+	printf("\n\nCPU_INFO\n\n");
+	short cpu_count = 0;
+	float cpu_user = 0;
+	float cpu_system = 0;
 
-        for (int i = 0; i < 10; i++) {
-                get_cpu_usage(&cpu_count, &cpu_user, &cpu_system);
-                printf("cpu_count = %d, cpu_user = %f, cpu_system = %f\n\n", cpu_count,
-                    cpu_user, cpu_system);
-                sleep(3);
-        }
+	for (int i = 0; i < 3; i++) {
+		get_cpu_usage(&cpu_count, &cpu_user, &cpu_system);
+		printf("cpu_count = %d, cpu_user = %f, cpu_system = %f\n\n", cpu_count,
+		    cpu_user, cpu_system);
+		sleep(3);
+	}
 
 
-        /*
-         * Get memory stats, works.
-         *
-         */
-        printf("\n\nMEM_INFO\n\n");
-        uint64_t memory_total, memory_free, memory_cache, memory_buffers, swap_total, swap_free;
-        get_memory_usage(&memory_total, &memory_free, &memory_cache,
-            &memory_buffers, &swap_total, &swap_free);
-        printf("memory_total = %llu, memory_free = %llu, memory_cache = %llu\n"
-            "memory_buffers = %llu, swap_total = %llu, swap_free = %llu\n",
-            memory_total, memory_free, memory_cache,
-            memory_buffers, swap_total, swap_free);
+	/*
+	 * Get memory stats, works.
+	 *
+	 */
+	printf("\n\nMEM_INFO\n\n");
+	uint64_t memory_total, memory_free, memory_cache, memory_buffers, swap_total, swap_free;
+	get_memory_usage(&memory_total, &memory_free, &memory_cache,
+	    &memory_buffers, &swap_total, &swap_free);
+	printf("memory_total = %" PRIu64 " memory_free = %" PRIu64 ", memory_cache = %" PRIu64 "\n"
+	    "memory_buffers = %" PRIu64 ", swap_total = %" PRIu64 ", swap_free = %" PRIu64 "\n",
+	    memory_total, memory_free, memory_cache,
+	    memory_buffers, swap_total, swap_free);
 
 	puts("finished...");
-        return 0;
+	return 0;
 
 }
